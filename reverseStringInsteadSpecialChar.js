c3dc89936b4df378deb5a969f43682055142340f
Rohan kumar <46267198+RohanGau@users.noreply.github.com>
2024-03-04 00:51:29 +0530

    Update reverseStringInsteadSpecialChar.js

--- reverseStringInsteadSpecialChar.js
+++ reverseStringInsteadSpecialChar.js
@@ -31,3 +31,42 @@ function reverse(str) {
 }
 console.log("before :", string);
 console.log("after :", reverse(string));
+
+
+function isAlphabet(char) {
+  return (char >= "A" && char <= "Z") || (char >= "a" && char <= "z");
+}
+
+function swap(str, a, b) {
+  var c = "";
+  for (let i = 0; i < str.length; i++) {
+    if (i === a) {
+      c = c + str[b];
+    } else if (i === b) {
+      c = c + str[a];
+    } else {
+      c = c + str[i];
+    }
+  }
+  return c;
+}
+
+function reverse(str) {
+  let l = 0;
+  let r = str.length - 1;
+  while (l < r) {
+    if (!isAlphabet(str[l])) {
+      l++;
+    } else if (!isAlphabet(str[r])) {
+      r--;
+    } else {
+      str = swap(str, l, r);
+      l++;
+      r--;
+    }
+  }
+  console.log("str  :", str);
+  return str;
+}
+console.log("before :", string);
+console.log("after :", reverse(string));
